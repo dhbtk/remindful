05a92e337463a63da203d0714a50c190be08e424
Diana Horbatiuk <diana@dianahorbatiuk.com>
2021-02-01 12:23:43 -0300

    fix migrations

--- db/migrate/20210125205451_add_extra_fields_to_users.rb
+++ db/migrate/20210125205451_add_extra_fields_to_users.rb
@@ -3,6 +3,5 @@ class AddExtraFieldsToUsers < ActiveRecord::Migration[6.1]
     add_column :users, :name, :string, null: false, default: ''
     add_column :users, :avatar_url, :string, null: false, default: ''
     add_column :users, :pronouns, :string, null: false, default: 'neutral'
-    change_column_null :users, :email, true
   end
 end
