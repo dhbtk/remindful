036efa245a5811e70c999922d81fbd72ba270ca3
Diana Horbatiuk <diana@dianahorbatiuk.com>
2021-02-26 11:06:19 -0300

    reload tasks after updating

--- app/javascript/src/store/tasks.ts
+++ app/javascript/src/store/tasks.ts
@@ -2,7 +2,7 @@ import { Task } from './common'
 import { createReducer } from '@reduxjs/toolkit'
 import { formatISO } from 'date-fns'
 import taskApi from '../api/taskApi'
-import { AppThunk } from './index'
+import { AppDispatch, AppThunk } from './index'
 import {
   bulkLoadTasks,
   loadDayData,
@@ -12,24 +12,38 @@ import {
   setTask,
   unsetTask
 } from './commonActions'
+import { RootState } from './rootReducer'
 
 export interface TasksState {
   entities: { [id: number]: Task }
   overdueIds: number[]
 }
 
+async function updateAndReload (task: Task, dispatch: AppDispatch, getState: () => RootState) {
+  dispatch(setTask(task))
+  const isOverdue = getState().tasks.overdueIds.includes(task.id)
+  if (isOverdue) {
+    const newIds = getState().tasks.overdueIds.filter(it => it !== task.id)
+    dispatch(reorderOverdueTasks(newIds))
+  }
+  await taskApi.update(task)
+  if (isOverdue) {
+    await dispatch(loadOverdueTasks())
+  } else {
+    await dispatch(loadTasks(task.eventDate))
+  }
+}
+
 export const completeTask: (id: number) => AppThunk = (id) => async (dispatch, getState) => {
   const task = getState().tasks.entities[id]
   const updatedTask: Task = { ...task, status: 'done', actedAt: formatISO(new Date()) }
-  dispatch(setTask(updatedTask))
-  await taskApi.update(updatedTask)
+  await updateAndReload(updatedTask, dispatch, getState)
 }
 
 export const dismissTask: (id: number) => AppThunk = (id) => async (dispatch, getState) => {
   const task = getState().tasks.entities[id]
   const updatedTask: Task = { ...task, status: 'dismissed', actedAt: formatISO(new Date()) }
-  dispatch(setTask(updatedTask))
-  await taskApi.update(updatedTask)
+  await updateAndReload(updatedTask, dispatch, getState)
 }
 
 export const undoCompleteTask: (id: number) => AppThunk = (id) => async (dispatch, getState) => {
